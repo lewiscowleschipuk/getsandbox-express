0f06e99488b79bd934265a59e2e89d44a45188aa
Wei Chen <wei.chen@d2l.com>
2016-04-15 17:59:06 -0400

    init

---
+++ sandbox.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var fs = require('fs');
+var vm = require('vm');
+var requireLike = require('require-like');
+var morgan = require('morgan');
+
+var app;
+var Sandbox = {};
+
+Sandbox.define = function define() {
+  var url, verb, handler;
+  if (arguments.length === 2) {
+    url = arguments[0];
+    verb = 'get';
+    handler = arguments[1];
+  } else if (arguments.length === 3) {
+    url = arguments[0];
+    verb = arguments[1];
+    handler = arguments[2];
+  }
+  verb = verb.toLowerCase();
+  url = url.replace(/{(.+?)}/g, ":$1");
+  url = encodeURI(url);
+
+  app[verb](url, handler);
+};
+
+var loadSandbox = function loadSandbox(expressApp, sandboxMainPath) {
+  if (!path.isAbsolute(sandboxMainPath)){
+    throw new Error('sandbox main path must be absolute path');
+  }
+  app = expressApp;
+  app.use(morgan('dev'));
+
+  var sandbox = {};
+  sandbox.require = requireLike(sandboxMainPath);
+  sandbox.Sandbox = Sandbox;
+  sandbox.state = {};
+  sandbox.moment = require('moment-timezone');
+
+  var script = new vm.Script(fs.readFileSync(sandboxMainPath).toString());
+  script.runInNewContext(sandbox);
+};
+
+module.exports = {
+  loadSandbox: loadSandbox
+};
